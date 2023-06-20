32ca3b46d069d828d8e00e9e1cfb408a0dd84440
Allan Robert Tan <argt@duck.com>
2023-06-20 16:47:00 +0800

    add give up button

--- script/game.js
+++ script/game.js
@@ -1,186 +1,195 @@
-import { getAdjacent, occurenceOf, delay } from "./helpers.js";
-import confetti from "./confetti.js";
-import chooseRandom from "./random.js";
+import { getAdjacent, occurenceOf, delay } from './helpers.js'
+import confetti from './confetti.js'
+import chooseRandom from './random.js'
 
 $(document).ready(() => {
-  let currentRow = 1;
-  let currentItem = 1;
-  let playerWon = false;
-  let gameActive = false;
-  let wordInput = "";
-  let word = "";
-  let wordsList = null;
-  let answers = null;
-  const green = "rgb(90,230,118)";
-  const yellow = "rgb(254,254,51)";
-  const gray = "rgb(172,172,172)";
+  let currentRow = 1
+  let currentItem = 1
+  let playerWon = false
+  let gameActive = false
+  let wordInput = ''
+  let word = ''
+  let wordsList = null
+  let answers = null
+  const green = 'rgb(90,230,118)'
+  const yellow = 'rgb(254,254,51)'
+  const gray = 'rgb(172,172,172)'
 
-  getRandomWord();
-  showMessage("Guess the first word", 1000);
+  getRandomWord()
+  showMessage('Guess the first word', 1000)
 
-  $.ajax("https://raw.githubusercontent.com/eillanrt/wordle-game/main/assets/words.txt")
-   .done((words) => {
-    wordsList = words.split("\n").map((line) => line.trim());
-  });
+  $.ajax(
+    'https://raw.githubusercontent.com/eillanrt/wordle-game/main/assets/words.txt'
+  ).done((words) => {
+    wordsList = words.split('\n').map((line) => line.trim())
+  })
 
   function getRandomWord() {
-    gameActive = false;
+    gameActive = false
 
     if (answers === null) {
-      $.ajax("https://raw.githubusercontent.com/eillanrt/wordle-game/main/assets/answers.txt").done((res) => {
-        answers = res.split("\n").map((line) => line.trim());
-        word = chooseRandom(answers);
-      });
+      $.ajax(
+        'https://raw.githubusercontent.com/eillanrt/wordle-game/main/assets/answers.txt'
+      ).done((res) => {
+        answers = res.split('\n').map((line) => line.trim())
+        word = chooseRandom(answers)
+      })
     } else {
-      word = chooseRandom(answers);
+      word = chooseRandom(answers)
     }
 
-    gameActive = true;
+    gameActive = true
   }
 
   function writeLetter(letter) {
-    letter = letter.toUpperCase();
+    letter = letter.toUpperCase()
 
-    if (currentItem === 5 && $(`#row${currentRow} .item`).last().text() !== "")
-      return;
+    if (currentItem === 5 && $(`#row${currentRow} .item`).last().text() !== '')
+      return
 
-    if (currentItem !== 5) currentItem++;
+    if (currentItem !== 5) currentItem++
 
-    wordInput += letter;
+    wordInput += letter
 
-    $(`#row${currentRow} .item`)
-      .each((i, el) => {
-        if ($(el).is(":empty")) $(el).text(wordInput[i]);
-      });
+    $(`#row${currentRow} .item`).each((i, el) => {
+      if ($(el).is(':empty')) $(el).text(wordInput[i])
+    })
   }
 
   function backspace() {
-    if (currentItem === 1 && $(`#row${currentRow} .item`).first().is(":empty"))
-      return;
+    if (currentItem === 1 && $(`#row${currentRow} .item`).first().is(':empty'))
+      return
 
-    if (currentItem !== 1) currentItem--;
+    if (currentItem !== 1) currentItem--
 
-    wordInput = wordInput.slice(0, -1);
+    wordInput = wordInput.slice(0, -1)
 
-    const lastLetter = wordInput.length;
-    $(`#row${currentRow} .item`).eq(lastLetter).empty();
+    const lastLetter = wordInput.length
+    $(`#row${currentRow} .item`).eq(lastLetter).empty()
   }
 
   async function enter() {
     if (wordInput.length !== 5) {
-      showMessage("Too Short");
-      return;
+      showMessage('Too Short')
+      return
     }
 
     if (!wordsList.includes(wordInput)) {
-      showMessage("Word not Found");
-      return;
+      showMessage('Word not Found')
+      return
     }
 
-    const adjacents = getAdjacent(word, wordInput);
+    const adjacents = getAdjacent(word, wordInput)
 
-    gameActive = false;
+    gameActive = false
     for (let i = 0; i < 5; i++) {
-      await delay(300);
-      const current = $(`#row${currentRow} .item`).eq(i);
+      await delay(300)
+      const current = $(`#row${currentRow} .item`).eq(i)
 
       if (occurenceOf(wordInput[i], word) > 1) {
-        current.append(`<sup>${occurenceOf(wordInput[i], word)}</sup>`);
+        current.append(`<sup>${occurenceOf(wordInput[i], word)}</sup>`)
       }
 
-      const letterKey = $(`#${wordInput[i].toLowerCase()}`);
+      const letterKey = $(`#${wordInput[i].toLowerCase()}`)
 
       if (adjacents.includes(i)) {
-        letterKey.css("background-color", green);
-        current.css("background-color", green);
+        letterKey.css('background-color', green)
+        current.css('background-color', green)
       } else if (word.includes(wordInput[i])) {
-        letterKey.css("background-color", yellow);
-        current.css("background-color", yellow);
+        letterKey.css('background-color', yellow)
+        current.css('background-color', yellow)
       } else {
-        letterKey.css("background-color", gray);
-        current.css("background-color", gray);
+        letterKey.css('background-color', gray)
+        current.css('background-color', gray)
       }
     }
 
     if (adjacents.length === 5) {
-      playerWon = true;
-      showWord();
+      playerWon = true
+      showWord()
 
-      return;
+      return
     }
-    gameActive = true;
-    wordInput = "";
-    currentItem = 1;
+    gameActive = true
+    wordInput = ''
+    currentItem = 1
     if (currentRow !== 6) {
-      currentRow++;
-      return;
+      currentRow++
+      return
     }
-    showWord();
+    showWord()
   }
 
   function playAgain() {
-    getRandomWord();
-    $(".item").empty().css("background-color", "transparent");
-    $(".letter").css("background-color", "lightgray");
-    $("#modalwin h1").empty();
-    currentItem = 1;
-    currentRow = 1;
-    playerWon = false;
-    wordInput = "";
+    getRandomWord()
+    $('.item').empty().css('background-color', 'transparent')
+    $('.letter').css('background-color', 'lightgray')
+    $('#modalwin h1').empty()
+    currentItem = 1
+    currentRow = 1
+    playerWon = false
+    wordInput = ''
   }
 
   async function confettiEffect() {
-    await delay(200);
-    confetti.start();
-    await delay(2000);
-    confetti.stop();
+    await delay(200)
+    confetti.start()
+    await delay(2000)
+    confetti.stop()
   }
 
   async function showMessage(message, duration = 700) {
-    gameActive = false;
-    $("#message h1").text(message);
-    $("#message").show();
+    gameActive = false
+    $('#message h1').text(message)
+    $('#message').show()
 
-    await delay(duration);
+    await delay(duration)
 
-    $("#message").hide();
-    $("#message h1").empty();
-    gameActive = true;
+    $('#message').hide()
+    $('#message h1').empty()
+    gameActive = true
   }
 
   function showWord() {
-    gameActive = false;
-    $("#modalwin").show();
-    $("#modalwin h1").text(word);
-    $("#modalwin span").on("click", () => {
-      $("#modalwin").hide();
-      playAgain();
-    });
+    gameActive = false
+    $('#modalwin').show()
+    $('#modalwin h1').text(word)
+    $('#modalwin span').on('click', () => {
+      $('#modalwin').hide()
+      playAgain()
+    })
 
-    if (playerWon) confettiEffect();
+    if (playerWon) confettiEffect()
   }
 
-  $(window).on("keydown", handleKeyDown);
-  $(".key").on("click", handleKeyClick);
+  $(window).on('keydown', handleKeyDown)
+  $('.key').on('click', handleKeyClick)
+  $('#give-up-btn').on('click', giveUp)
 
   // event handlers
   function handleKeyClick(event) {
-    const gameIsReady = gameActive && word !== "" && wordsList !== null;
-    if (!gameIsReady) return;
+    const gameIsReady = gameActive && word !== '' && wordsList !== null
+    if (!gameIsReady) return
 
-    const { id } = event.target;
+    const { id } = event.target
 
-    if (id === "enter") enter();
-    else if (id === "backspace") backspace();
-    else writeLetter(id);
+    if (id === 'enter') enter()
+    else if (id === 'backspace') backspace()
+    else writeLetter(id)
   }
 
   function handleKeyDown(event) {
-    const gameIsReady = gameActive && word !== "" && wordsList !== null;
-    if (!gameIsReady) return;
+    const gameIsReady = gameActive && word !== '' && wordsList !== null
+    if (!gameIsReady) return
 
-    if (event.code === "Enter") enter();
-    else if (event.code === "Backspace") backspace();
-    else if (event.code.startsWith("Key")) writeLetter(event.key);
+    if (event.code === 'Enter') enter()
+    else if (event.code === 'Backspace') backspace()
+    else if (event.code.startsWith('Key')) writeLetter(event.key)
   }
-});
+
+  function giveUp() {
+    if (confirm('Are you sure you want to give up?')) {
+      showWord()
+    }
+  }
+})
